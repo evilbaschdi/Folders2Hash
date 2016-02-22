e08ab276ada68586a76dce3014f15e79216debd8
evilbaschdi <sebwalter@gmx.net>
2016-02-22 19:55:01 +0100

    fallback

--- Folders2Md5/Internal/Calculate.cs
+++ Folders2Md5/Internal/Calculate.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Security.Cryptography;
+using System.Text;
 
 namespace Folders2Md5.Internal
 {
@@ -8,14 +9,59 @@ namespace Folders2Md5.Internal
     {
         public string Hash(string filename, string type)
         {
-            using (var fs = new FileStream(filename, FileMode.Open, FileAccess.Read))
+            switch (type.ToLower())
             {
-                //todo: SHA256 aufnehmen
-                //todo: checkboxlist für sämtliche möglichen hash algorithmen
-                var hash = ((HashAlgorithm) CryptoConfig.CreateFromName(type.ToUpper())).ComputeHash(fs);
-                var converted = BitConverter.ToString(hash).Replace("-", string.Empty);
+                case "md5":
+                    return Md5Hash(filename);
+                case "sha1":
+                    return Sha1Hash(filename);
+            }
+            return "type not found";
+        }
 
-                return converted;
+        private string Md5Hash(string filename)
+        {
+            try
+            {
+                var md5 = MD5.Create();
+                using (var fs = new FileStream(filename, FileMode.Open, FileAccess.Read))
+                {
+                    var hash = md5.ComputeHash(fs);
+
+                    var sb = new StringBuilder();
+                    foreach (var t in hash)
+                    {
+                        sb.Append(t.ToString("X2"));
+                    }
+                    return sb.ToString();
+                }
+            }
+            catch (Exception exception)
+            {
+                return exception.Message;
+            }
+        }
+
+        private string Sha1Hash(string filename)
+        {
+            try
+            {
+                var sha1 = SHA1.Create();
+                using (var fs = new FileStream(filename, FileMode.Open, FileAccess.Read))
+                {
+                    var hash = sha1.ComputeHash(fs);
+
+                    var sb = new StringBuilder();
+                    foreach (var t in hash)
+                    {
+                        sb.Append(t.ToString("X2"));
+                    }
+                    return sb.ToString();
+                }
+            }
+            catch (Exception exception)
+            {
+                return exception.Message;
             }
         }
     }
